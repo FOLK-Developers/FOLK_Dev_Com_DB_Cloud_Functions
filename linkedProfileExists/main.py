ae2bc83f8ad36e2e875e5a4d02ea897822b57776
Sahil Saini <55438580+Sahil1515@users.noreply.github.com>
2021-04-03 10:56:51 +0530

    Update main.py

--- linkedProfileExists/main.py
+++ linkedProfileExists/main.py
@@ -1,126 +1,167 @@
-from datetime import time
 
+import time
 import firebase_admin
-import requests
-from firebase_admin import credentials
 from firebase_admin import firestore
 import flask
 from flask import request, jsonify
+from firebase_admin import auth
 
-# initialize firebase application
+# from firebase_admin import credentials
+# cred = credentials.Certificate('../folk-bf69e-firebase-adminsdk-3bcpx-173db28c66.json')
+# firebase_admin.initialize_app(cred)
+# db = firestore.client()
+
+
 firebase_admin.initialize_app()
-
-# connect to db
 db = firestore.client()
 
-# global variables
-recData = {'source': "Social Media",
-           'url': "email@gmail.com",
-           'domain': "google",
-           'secondary_source': "Facebook",
-           'name': "Unknown",
-           'signin_status': True,
-           'performer': "Admin",
-           'uid': "xyz"}
 
 
-# to get profile flags
-def getProfileFlags(doc_id):
-    docref = db.collection('Profile').document(doc_id)
-    data = docref.get().to_dict()
-    preprimary_infostatus = False
-    primary_infostatus = False
-    secondary_infostatus = False
-    tertiary_infostatus = False
-    name = 'Unknown Friend'
-    if 'flags' in data.keys():
-        if 'preprimary_infostatus' in data.keys()['flags']:
-            preprimary_infostatus = data['flags']['preprimary_infostatus']
-        if 'primary_infostatus' in data.keys()['flags']:
-            primary_infostatus = data['flags']['primary_infostatus']
-        if 'secondary_infostatus' in data.keys()['flags']:
-            secondary_infostatus = data['flags']['secondary_infostatus']
-        if 'tertiary_infostatus' in data.keys()['flags']:
-            tertiary_infostatus = data['flags']['tertiary_infostatus']
-    if 'name' in data.keys():
-        name = data['name']
-    return {"name": name, "preprimary_infostatus": preprimary_infostatus, 'secondary_infostatus': secondary_infostatus,
-            'primary_infostatus': primary_infostatus, 'tertiary_infostatus': tertiary_infostatus}
 
+def getProfileFlags(url,domain):
+    ProfileCol=None
+    if domain=="phone":
+        url = url.strip()
+        if url.startswith("+91") or url.startswith("91"):
+            url_p = url[-10:]
+        else:
+            url_p=url
+        ProfileCol = db.collection('Profile').where("mobile",'==',url_p)
+    elif domain=='google.com':
+        ProfileCol = db.collection('Profile').where("email",'==',url)
+    
+    preprimary_infostatus1=False
+    for pDoc in ProfileCol.stream():
+        ldoc_id=pDoc.id
+        ProfileDocExistsStatus1=True
+        data = pDoc.to_dict()
+        
+        if 'flags' in data.keys():
+            if 'pre_primary_info' in data['flags']:
+                preprimary_infostatus1 = data['flags']['pre_primary_info']
+            if 'primary_info' in data['flags']:
+                 preprimary_infostatus1= data['flags']['primary_info']
+
+    return {"preprimary_infostatus": preprimary_infostatus1, "profile_exist":ProfileDocExistsStatus1,"ldoc_id":ldoc_id}
 
 def hello_world(request):
     recData = flask.request.json
-    # print("Received Data :", recdata)
 
+    url_p=None
+    uid="None"
+    verification_status="Pending"
+    print(recData)
+
+    ProfileDocExistsStatus=False
     domain = recData['domain']
     url = recData['url']
-    secondary_source = recData['secondary_source']
-    primary_source = recData['primary_source']
-    name = recData['name']
-    signin_status = recData['signin_status']
-    performer = recData['performer']
-    uid = recData['uid']
-    # user_name = recData['name']
-
-    # your logic or code here..
-
-    docref = db.collection_group(u'LinkedProfiles') \
-        .where(u'domain', u'==', domain) \
-        .where('url', '==', url)
-    docs = docref.stream()
 
     ldoc_id = ""
     vstatus = ""
     type = ""
     lpvstatus = False
+    json_data=None
 
+    if domain == 'phone' or domain == 'google.com':
+        type = "communication"
+    elif domain == 'facebook' or domain == 'github' or domain == 'linkedin':
+        type = "social"
+
+    docref = db.collection(u'LinkedProfiles') \
+        .where(u'domain', u'==', domain) \
+        .where('url', '==', url)
+    docs = docref.stream()
+
+    json_data=getProfileFlags(url,domain)
+
+
+
+# If the Linked Profile is there and is verified then simply setting lpvstatus=True and returning
     for doc in docs:
-        # print(f'{doc.id} => {doc.to_dict()}')
-        ldoc_id = doc.to_dict()['doc_id']
+        ldoc_id = doc.to_dict()['user_doc_id']
         vstatus = doc.to_dict()['verification_status']
+    
+    # preprimary_infostatus = False
 
     if ldoc_id != "":
         if vstatus == "Verified":
             lpvstatus = True
+
+# If LinkedProfile is not there
     else:
-        docref = db.collection('Profile').document()
-        docref.set({
-            'secondary_source': secondary_source,
-            'primary_source': primary_source,
-            'name': name,
-            'timestamps.preprimary_info': int(time.time()),
-            'flags.preprimary_infostatus': True,
-            'user_access_level': ['public']
-        })
-        ldoc_id = docref.id
-        if domain == 'mobile' or domain == 'email':
-            type = "communication"
-        elif domain == 'facebook' or domain == 'github' or domain == 'linkedin':
-            type = "social"
+                        
+        if domain=='phone':
+            modified_url=url
+            try:
+                user = auth.get_user_by_phone_number(modified_url)
+                print(user.uid)
+                vstatus='Verified'
+                uid=user.uid
+            except:
+                print("Phone number not there!")
+        elif domain=='google.com':
+            try:
+                user = auth.get_user_by_email(url)
+                print(user.uid)
+                vstatus='Verified'
+                uid=user.uid
+            except:
+                print("Email not there!")
 
-        data = {'doc_id': ldoc_id,
-                'url': url,
-                'domain': domain,
-                'type': type,
-                'user_name': name,
-                'signin_status': signin_status,
-                'performer': performer,
-                'uid': uid}
 
-        lpvstatus = signin_status
-        resp = requests.post('https://us-central1-folk-database.cloudfunctions.net/createLinkedProfile',
-                             json=data)
 
-    flagsData = getProfileFlags(ldoc_id)
+
+        # If profile does not exist then create it
+        ProfileDocExistsStatus=recData["profile_exist"]
+        if ProfileDocExistsStatus ==False:
+            dat={}
+            if domain=='phone':
+                dat={
+                     "source":recData['source'],
+                    "time_stamps":{"origin":int(time.time())},
+                    "mobile":url
+                }
+            else:
+                dat={
+                    "time_stamps":{"origin":int(time.time())},
+                     "source":recData['source'],
+                    "email":url
+                }
+            docref = db.collection('Profile').document()
+            docref.set(
+                dat
+            ,merge=True)
+
+            print("Creating Profile")
+
+            ldoc_id = docref.id
+
+
+    #Creating the Linked Profile
+
+        data = {
+            "username":"Unknown",
+            'verification_status':verification_status ,
+            'verified_by': "None",
+            'verified_timestamp': int(time.time()),  # Setting current by default
+            'uid': uid,
+            'type': type,
+            'update_timestamp': int(time.time()),  # Setting current by default
+            'user_doc_id': json_data["ldoc_id"],
+            'domain': domain,
+            'url': url,
+            "source":recData['source']
+        }
+        docref = db.collection('LinkedProfiles').document()
+        docref.set(data)
+        print("Creating Linked profile")
+
+
 
     response = {
         "verification_status": lpvstatus,
-        "name": name,
-        "preprimary_infostatus": flagsData['preprimary_infostatus'],
-        'secondary_infostatus': flagsData['secondary_infostatus'],
-        'primary_infostatus': flagsData['primary_infostatus'],
-        'tertiary_infostatus': flagsData['tertiary_infostatus'],
-        "doc_id": ldoc_id
+        "preprimary_infostatus": json_data["preprimary_infostatus"],
+        "user_doc_id":  json_data["ldoc_id"],
     }
 
     data = {
@@ -130,14 +171,70 @@ def hello_world(request):
         'sub_category': "linkedProfileExists",
         'details': domain,
         'sub_details': url,
-        'performer': performer,
+        'performer': "user",
         'edate': int(time.time()),
-        'doc_id': ldoc_id,
+        'doc_id':  json_data["ldoc_id"],
         'readable_date': firestore.SERVER_TIMESTAMP,
         'access_permission': ['admin']
     }
 
-    docref = db.collection('Profile').document(ldoc_id).collection('Activities').document()
+    docref = db.collection('Profile').document( json_data["ldoc_id"]).collection('activities').document()
     docref.set(data)
 
+    print(response)
+
     return jsonify(response)
+
+
+
+
+
+
+
+# hello_world({"source":
+#     {"Zone":"VCM","Location":"Temple Desk","Campaign":"yoga_for_happiness","Referrer":"cGHgvv85qrbe16RDohXk","Medium":"whatsapp"},
+#     "url":"shikharag.sa2002@gmail.com",
+#     "domain":"google.com"
+#     })
+
+
+# recData = {'source':
+#                {
+#                 "Zone": "VCM",
+#                 "Location":"Temple Desk",
+#                 "Campaign":"yoga_for_happiness",
+#                 "Referrer":"cGHgvv85qrbe16RDohXk",
+#                 "Medium":"whatsapp"
+#                 },
+#            'url': "sahilsaini123@gmail.com",
+#            'domain': "google.com"
+# }
+
+# recData = {'source':
+#                {
+#                 "Zone": "VCM",
+#                 "Location":"Temple Desk",
+#                 "Campaign":"yoga_for_happiness",
+#                 "Referrer":"cGHgvv85qrbe16RDohXk",
+#                 "Medium":"whatsapp"
+#                 },
+#            'url': "968266365x",
+#            'domain': "phone"
+# }
+
+
+
+
+
+# hello_world(recData)
+
+
+
+# Three cases:
+#     profile exists:
+#             linked profile does not exist
+#             linked profile exists
+#     Profile does not exist
+
+
+
