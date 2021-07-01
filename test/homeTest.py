682b31e2376fbfd48d434814fec026f3b0895840
KISLAYA SRIVASTAVA <akshat.sri2605@gmail.com>
2021-07-01 23:25:04 +0530

    FA-204 #time 2h 30m Reading docs and understanding the folk app engine structure, rest api, postman testing, api using flask, etc

--- test/homeTest.py
+++ test/homeTest.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import firestore 
 
 
 cred = credentials.Certificate('./../folk-dev-com-db-firebase-adminsdk-mz02x-20922898af.json')
