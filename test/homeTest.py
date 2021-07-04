13e963191ad2944efa48af139b83e8c9b7913766
KISLAYA SRIVASTAVA <akshat.sri2605@gmail.com>
2021-07-04 18:33:42 +0530

    FA-204 #time 40m (3rd July)-Researched about batch update in SQL instance.

--- test/homeTest.py
+++ test/homeTest.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore 
-
+ 
  
 cred = credentials.Certificate('./../folk-dev-com-db-firebase-adminsdk-mz02x-20922898af.json')
 
