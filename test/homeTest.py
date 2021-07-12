014ae666c600cd4d333ffe39ccc6dc41da94cbf9
KISLAYA SRIVASTAVA <akshat.sri2605@gmail.com>
2021-07-12 23:56:01 +0530

    FA-204 #time 1h Researching about calling cloud function from local machine.

--- test/homeTest.py
+++ test/homeTest.py
@@ -6,7 +6,7 @@ from firebase_admin import firestore
 cred = credentials.Certificate('./../folk-dev-com-db-firebase-adminsdk-mz02x-20922898af.json') 
  
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://folk-database.firebaseio.com/'
+    'databaseURL': 'https://folk-database.firebaseio.com/' 
 })   
 # # token = [] 
 db = firestore.client()    
