590a97a95f8024c582ed5c77d9ab3eb704135375
KISLAYA SRIVASTAVA <akshat.sri2605@gmail.com>
2021-07-13 13:14:31 +0530

    FA-204 #time 1h Meeting with SaiTeja sir

--- test/homeTest.py
+++ test/homeTest.py
@@ -5,7 +5,7 @@ from firebase_admin import firestore
  
 cred = credentials.Certificate('./../folk-dev-com-db-firebase-adminsdk-mz02x-20922898af.json') 
  
-firebase_admin.initialize_app(cred, {
+firebase_admin.initialize_app(cred, { 
     'databaseURL': 'https://folk-database.firebaseio.com/' 
 })   
 # # token = [] 
